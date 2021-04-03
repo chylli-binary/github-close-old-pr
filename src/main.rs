71b85d82e9eb586254ae7614eeb9c75efc89a152
chylli-binary <chylli@binary.com>
2021-04-03 10:08:02 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use octocrab;
+#[tokio::main]
+async fn main() -> octocrab::Result<()> {
+    let diff = octocrab::instance()
+        .pulls("rust-lang", "rust")
+        .get_diff(72033)
+        .await?;
+
+    println!("{}", diff);
+
+    Ok(())
+}
